83dfbc35dd675d81c44b8f57a942e094416cf4d0
fakeryrelay <79545766+fakeryrelay@users.noreply.github.com>
2022-08-15 22:30:05 +0400

    Property Service v1.0.2
    1. Added a modal window to fill out the form
    2. Added simple animation for modal windows
    3. Adapted site for landscape mobile version
    4. Adapted images modal windows for landscape mobile version
    5. Added contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,13 +19,23 @@ eval("const $burger = document.getElementById('burger')\r\nconst $mobileNav = do
 
 /***/ }),
 
+/***/ "./src/assets/js/form-modal.js":
+/*!*************************************!*\
+  !*** ./src/assets/js/form-modal.js ***!
+  \*************************************/
+/***/ (function() {
+
+eval("const $formButton = document.querySelectorAll('[data-modal]')\r\nconst $formModal = document. querySelector('.form__modal')\r\nconst $modalInner = document.querySelector('modal')\r\nconst $body = document.body\r\nconst $sendBtn = document.getElementById('sendBtn')\r\n\r\n$formButton.forEach(item => {\r\n  item.addEventListener('click' ,event => {\r\n    let modalContent = $formModal.querySelector('.modal__content')\r\n\r\n    modalContent.addEventListener('click', event => {\r\n      event.stopPropagation()\r\n    })\r\n    \r\n    showFormModal()\r\n  })\r\n})\r\n\r\n$formModal.addEventListener('click', event => {\r\n  hideFormModal()\r\n})\r\n\r\n$sendBtn.addEventListener('click', event => {\r\n  hideFormModal()\r\n})\r\n\r\nfunction showFormModal() {\r\n  let modalContent = $formModal.querySelector('.modal__content')\r\n\r\n  setTimeout(() => {\r\n    modalContent.style.transform = 'translate(-50%, -50%)'\r\n    modalContent.style.opacity = '1'\r\n  }, 1)\r\n\r\n  $formModal.classList.add('active')\r\n  $body.classList.add('no-scroll')\r\n}\r\n\r\nfunction hideFormModal() {\r\n  let modalContent = $formModal.querySelector('.modal__content')\r\n  \r\n  modalContent.style.transform = 'translate(-50%, -60%)'\r\n  modalContent.style.opacity = '0'\r\n  \r\n  setTimeout(() => {\r\n    $formModal.classList.remove('active')\r\n    $body.classList.remove('no-scroll')\r\n  }, 200)\r\n}\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/form-modal.js?");
+
+/***/ }),
+
 /***/ "./src/assets/js/image-modal.js":
 /*!**************************************!*\
   !*** ./src/assets/js/image-modal.js ***!
   \**************************************/
 /***/ (function() {
 
-eval("const $imageItems = document.querySelectorAll('.gallery__img')\r\nconst $imageModal = document.querySelector('.image__modal')\r\nconst $modalInner = document.querySelector('modal')\r\nconst $modalImg = document.querySelector('.modal__img')\r\nconst $body = document.body\r\n\r\nconsole.log($imageItems)\r\n\r\n$imageItems.forEach(item => {\r\n  item.addEventListener('click', event => {\r\n    let $this = event.currentTarget\r\n    let link = $this.getAttribute('src')\r\n    let modalContent = $imageModal.querySelector('.modal__content')\r\n\r\n    modalContent.addEventListener('click', event => {\r\n      event.stopPropagation()\r\n    })\r\n\r\n    showImageModal(link)\r\n  })\r\n})\r\n\r\n$imageModal.addEventListener('click', event => {\r\n  hideImageModal()\r\n})\r\n\r\nfunction showImageModal(link) {\r\n  $modalImg.setAttribute('src', `${link}`)\r\n  $imageModal.classList.add('active')\r\n  $body.classList.add('no-scroll')\r\n  \r\n  console.log($modalImg)\r\n}\r\n\r\nfunction hideImageModal() {\r\n  $imageModal.classList.remove('active')\r\n  $body.classList.remove('no-scroll')\r\n}\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/image-modal.js?");
+eval("const $imageItems = document.querySelectorAll('.gallery__img')\r\nconst $imageModal = document.querySelector('.image__modal')\r\nconst $modalInner = document.querySelector('modal')\r\nconst $modalImg = document.querySelector('.modal__img')\r\nconst $body = document.body\r\n\r\n$imageItems.forEach(item => {\r\n  item.addEventListener('click', event => {\r\n    let $this = event.currentTarget\r\n    let link = $this.getAttribute('src')\r\n    let modalContent = $imageModal.querySelector('.modal__content')\r\n\r\n    modalContent.addEventListener('click', event => {\r\n      event.stopPropagation()\r\n    })\r\n\r\n    showImageModal(link)\r\n  })\r\n})\r\n\r\n$imageModal.addEventListener('click', event => {\r\n  hideImageModal()\r\n})\r\n\r\nfunction showImageModal(link) {\r\n  let modalContent = $imageModal.querySelector('.modal__content')\r\n\r\n  setTimeout(() => {\r\n    modalContent.style.transform = 'translate(-50%, -50%)'\r\n    modalContent.style.opacity = '1'\r\n  }, 1)\r\n  \r\n  $modalImg.setAttribute('src', `${link}`)\r\n  $imageModal.classList.add('active')\r\n  $body.classList.add('no-scroll')\r\n\r\n}\r\n\r\nfunction hideImageModal() {\r\n  let modalContent = $imageModal.querySelector('.modal__content')\r\n  \r\n  modalContent.style.transform = 'translate(-50%, -60%)'\r\n  modalContent.style.opacity = '0'\r\n  \r\n  setTimeout(() => {\r\n    $imageModal.classList.remove('active')\r\n    $body.classList.remove('no-scroll')\r\n  }, 200)\r\n}\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/image-modal.js?");
 
 /***/ }),
 
@@ -35,7 +45,7 @@ eval("const $imageItems = document.querySelectorAll('.gallery__img')\r\nconst $i
   \**************************************/
 /***/ (function() {
 
-eval("const $page = document.querySelector('.page')\r\nconst $preview = document.querySelector('.project-preview__video-inner')\r\nconst $videoModal = document.querySelector('.video__modal')\r\nconst $body = document.body\r\n\r\n\r\n$preview.addEventListener('click', event => {\r\n  let modalContent = $videoModal.querySelector('.modal__content')\r\n\r\n  modalContent.addEventListener('click', event => {\r\n    event.stopPropagation()\r\n  })\r\n  showVideoModal()\r\n})\r\n\r\n$videoModal.addEventListener('click', event => {\r\n  hideVideoModal()\r\n})\r\n\r\nfunction showVideoModal() {\r\n  $videoModal.classList.add('active')\r\n  $body.classList.add('no-scroll')\r\n}\r\n\r\nfunction hideVideoModal() {\r\n  $videoModal.classList.remove('active')\r\n  $body.classList.remove('no-scroll')\r\n}\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/video-modal.js?");
+eval("const $page = document.querySelector('.page')\r\nconst $preview = document.querySelectorAll('.project-preview__video-inner')\r\nconst $videoModal = document.querySelector('.video__modal')\r\nconst $body = document.body\r\n\r\n$preview.forEach(item => {\r\n  item.addEventListener('click', event => {\r\n    let modalContent = $videoModal.querySelector('.modal__content')\r\n  \r\n    modalContent.addEventListener('click', event => {\r\n      event.stopPropagation()\r\n    })\r\n    \r\n    showVideoModal()\r\n  })\r\n})\r\n\r\n\r\n$videoModal.addEventListener('click', event => {\r\n  hideVideoModal()\r\n})\r\n\r\nfunction showVideoModal() {\r\n  let modalContent = $videoModal.querySelector('.modal__content')\r\n\r\n  setTimeout(() => {\r\n    modalContent.style.transform = 'translate(-50%, -50%)'\r\n    modalContent.style.opacity = '1'\r\n  }, 1)\r\n\r\n  $videoModal.classList.add('active')\r\n  $body.classList.add('no-scroll')\r\n}\r\n\r\nfunction hideVideoModal() {\r\n  let modalContent = $videoModal.querySelector('.modal__content')\r\n  \r\n  modalContent.style.transform = 'translate(-50%, -60%)'\r\n  modalContent.style.opacity = '0'\r\n  setTimeout(() => {\r\n    $videoModal.classList.remove('active')\r\n    $body.classList.remove('no-scroll')\r\n  })\r\n  \r\n}\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/video-modal.js?");
 
 /***/ })
 
@@ -47,8 +57,9 @@ eval("const $page = document.querySelector('.page')\r\nconst $preview = document
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	__webpack_modules__["./src/assets/js/app.js"]();
 /******/ 	__webpack_modules__["./src/assets/js/image-modal.js"]();
-/******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/assets/js/video-modal.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/form-modal.js"]();
 /******/ 	
 /******/ })()
 ;
